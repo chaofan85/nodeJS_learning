14c97007d99c57bdd6a58434ce8d42edefa4d57a
Chao Fan <fanchao05@gmail.com>
2018-02-13 22:06:59 -0500

    added app.js

---
+++ weather-app/app.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+request({
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=8015%20grenfell%20street%20kew%20gardens',
+  json: true
+}, (error, response, body) => {
+  console.log(JSON.stringify(body, undefined, 2));
+});
