0d6e11797dd5f356d19936c3d1113833b62d778d
Chao Fan <fanchao05@gmail.com>
2018-02-14 13:39:49 -0500

    added weather api call

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,24 +1,47 @@
+const request = require('request');
+
 const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode.js');
+//
+// const argv = yargs
+//     .options({
+//       a: {
+//         demand: true,
+//         alias: 'address',
+//         describe: 'Address to fetch weather for',
+//         string: true
+//       }
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
 
-const geocode = require('./geocode/geocode.js');
-
+const API_KEY = "0a9715788710ee5fbbd990da86dae70d";
 const argv = yargs
     .options({
-      a: {
+      lag: {
         demand: true,
-        alias: 'address',
-        describe: 'Address to fetch weather for',
-        string: true
+        alias: "latitude",
+      },
+      lng: {
+        demand: true,
+        alias: "longitude"
       }
     })
-    .help()
-    .alias('help', 'h')
     .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
-  } else {
-    console.log(JSON.stringify(results, undefined, 2));
-  }
+request({
+  url: `https://api.darksky.net/forecast/${API_KEY}/${argv.lag},${argv.lng}`,
+  json: true
+}, (error, response, body) => {
+  console.log(`Temperature: ${body.currently.temperature}`);
 });
