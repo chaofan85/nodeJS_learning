0de3b037ce2f25c437ef46d2389a0693f82aca47
Chao Fan <fanchao05@gmail.com>
2018-02-14 23:37:17 -0500

    updated code

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,44 +3,31 @@ const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
 const weather = require('./weather/weather');
 
-// const argv = yargs
-//     .options({
-//       a: {
-//         demand: true,
-//         alias: 'address',
-//         describe: 'Address to fetch weather for',
-//         string: true
-//       }
-//     })
-//     .help()
-//     .alias('help', 'h')
-//     .argv;
-
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   if (errorMessage) {
-//     console.log(errorMessage);
-//   } else {
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-// });
-
 const argv = yargs
     .options({
-      lag: {
+      a: {
         demand: true,
-        alias: "latitude",
-      },
-      lng: {
-        demand: true,
-        alias: "longitude"
+        alias: 'address',
+        describe: 'Address to fetch weather for',
+        string: true
       }
     })
+    .help()
+    .alias('help', 'h')
     .argv;
 
-weather.getWeather(argv.lag, argv.lng, (errorMessage, results) => {
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   if (errorMessage) {
     console.log(errorMessage);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude,
+    (errorMessage2, weatherResults) => {
+      if (errorMessage) {
+        console.log(errorMessage2);
+      } else {
+        console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}`);
+      }
+    });
   }
 });
