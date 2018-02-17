e8a9ba949164ee7445dafc9a11e571c52aaf2bf7
Chao Fan <fanchao05@gmail.com>
2018-02-17 18:36:16 -0500

    updated test case

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,25 @@
+const expect = require('expect');
 const utils = require('./utils');
+
 
 it('should add two numbers', () => {
   const res = utils.add(33, 11);
 
-  if (res !== 44) {
-     throw new Error(`Expected 44, but got ${res}.`);
-  }
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
   const res = utils. square(4);
 
-  if (res !== 16) {
-    throw new Error(`Expected 16, but got ${res}.`);
-  }
+  expect(res).toBe(16).toBeA('number');
+});
+
+it('should set first name and last name', () => {
+  let user = {location: 'New York', age: 32};
+  const res = utils.setName(user, 'Chao Fan');
+
+  expect(res).toInclude({
+    firstName: 'Chao',
+    lastName: 'Fan'
+  });
 });
