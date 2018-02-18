2e676a9ee62c52f61cbec584cb5b1a2fa8eb223b
Chao Fan <fanchao05@gmail.com>
2018-02-18 18:59:40 -0500

    did query practice

--- todo-api/playground/mongodb-find.js
+++ todo-api/playground/mongodb-find.js
@@ -8,13 +8,28 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   console.log(('Connected to MongoDB server'));
   const db = client.db('TodoApp');
 
-  db.collection('Todos').find({
-    _id: new ObjectID('5a88e1269c7bfe4dce9109f8')
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5a88e1269c7bfe4dce9109f8')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (error) => {
+  //   console.log('Unable to fetch todos', error);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (error) => {
+  //   console.log('Unable to fetch todos', error);
+  // });
+
+  db.collection("Users").find({
+    age: 64
   }).toArray().then((docs) => {
-    console.log('Todos');
+    console.log('Users');
     console.log(JSON.stringify(docs, undefined, 2));
   }, (error) => {
-    console.log('Unable to fetch todos', error);
+    console.log('Unable to fetch data', error);
   });
 
   // client.close();
