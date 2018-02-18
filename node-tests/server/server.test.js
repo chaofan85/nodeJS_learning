446e9cffa303025ecdb8a1dbeac23bba35cae192
Chao Fan <fanchao05@gmail.com>
2018-02-17 19:48:34 -0500

    refactored test cases

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -3,27 +3,34 @@ const expect = require('expect');
 
 const app = require('./server').app;
 
-it('should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: 'Page not found.'
-      });
-    })
-    .end(done);
-});
+describe('server', () => {
+  describe('GET /', () => {
+    it('should return hello world response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            error: 'Page not found.'
+          });
+        })
+        .end(done);
+    });
+  });
 
-it('should contain Chao Fan', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'Chao Fan',
-        age: 32
-      });
-    })
-    .end(done);
+  describe('GET /users', () => {
+    it('should contain Chao Fan', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            name: 'Chao Fan',
+            age: 32
+          });
+        })
+        .end(done);
+    });
+  });
+
 });
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,39 +1,43 @@
 const expect = require('expect');
 const utils = require('./utils');
 
+describe('Utils', () => {
 
-it('should add two numbers', () => {
-  const res = utils.add(33, 11);
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      const res = utils.add(33, 11);
 
-  expect(res).toBe(44).toBeA('number');
-});
-
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
+      expect(res).toBe(44).toBeA('number');
+    });
   });
-});
 
-it('should square a number', () => {
-  const res = utils. square(4);
+  it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+      expect(sum).toBe(7).toBeA('number');
+      done();
+    });
+  });
 
-  expect(res).toBe(16).toBeA('number');
-});
+  it('should square a number', () => {
+    const res = utils. square(4);
 
-it('should async add two numbers', (done) => {
-  utils.asyncSquare(4, (res) => {
     expect(res).toBe(16).toBeA('number');
-    done();
   });
-});
 
-it('should set first name and last name', () => {
-  let user = {location: 'New York', age: 32};
-  const res = utils.setName(user, 'Chao Fan');
+  it('should async add two numbers', (done) => {
+    utils.asyncSquare(4, (res) => {
+      expect(res).toBe(16).toBeA('number');
+      done();
+    });
+  });
 
-  expect(res).toInclude({
-    firstName: 'Chao',
-    lastName: 'Fan'
+  it('should set first name and last name', () => {
+    let user = {location: 'New York', age: 32};
+    const res = utils.setName(user, 'Chao Fan');
+
+    expect(res).toInclude({
+      firstName: 'Chao',
+      lastName: 'Fan'
+    });
   });
 });
