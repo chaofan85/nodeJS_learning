e4aa2ed763bb5ae5285dce40d5fe450666dd3f7b
Chao Fan <fanchao05@gmail.com>
2018-02-18 18:48:00 -0500

    added mongodb-find.js

--- todo-api/playground/mongodb-connect.js
+++ todo-api/playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   if (err) {
@@ -18,17 +19,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
 
-  db.collection('Users').insertOne({
-    name: 'Chao Fan',
-    age: 32,
-    location: 'New York'
-  }, (error, result) => {
-    if (error) {
-      return console.log('Unable to insert user profile.', error);
-    }
-
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
+  // db.collection('Users').insertOne({
+  //   name: 'Chao Fan',
+  //   age: 32,
+  //   location: 'New York'
+  // }, (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert user profile.', error);
+  //   }
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
   client.close();
 });
