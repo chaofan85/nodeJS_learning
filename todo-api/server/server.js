84c87b27f1618b98c49b472ac8ec8eb3bce41864
Chao Fan <fanchao05@gmail.com>
2018-02-18 20:05:13 -0500

    added mongoose and server.js

---
+++ todo-api/server/server.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+const Todo = mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
+
+// const newTodo = new Todo({
+//   text: 'Cook dinner'
+// });
+
+const newTodo = new Todo({
+  text: 'Get haircut',
+  completed: false,
+  completedAt: 20180218
+});
+
+newTodo.save().then((doc) => {
+  console.log('Saved todo', doc);
+}, (e) => {
+  console.log('Unable to save todo.');
+});
