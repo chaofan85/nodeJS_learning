d9d660014fabdd565ba1f9dc09324982bc3301ae
Chao Fan <fanchao05@gmail.com>
2018-02-19 17:12:11 -0500

    added another test case

--- todo-api/server/tests/server.test.js
+++ todo-api/server/tests/server.test.js
@@ -31,4 +31,17 @@ describe('POST /todos', () => {
         }).catch((e) => done(e));
       });
   });
+
+  it('should not create todo with invalid body data', (done) => {
+
+    request(app)
+      .post('/todos')
+      .send('')
+      .expect(400);
+
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(0);
+        done();
+      }).catch((e) => done(e));
+  });
 });
