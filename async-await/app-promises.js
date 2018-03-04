b4dde78399acfe23752eff8204b75f5a51708f90
Chao Fan <fanchao05@gmail.com>
2018-03-04 16:16:20 -0500

    added new exercise

---
+++ async-await/app-promises.js
@@ -0,0 +1,29 @@
+const users = [{
+  id: 1,
+  name: 'Chao',
+  schoolId: 101
+}, {
+  id: 2,
+  name: 'Randy',
+  schoolId: 999
+}];
+
+const grades = [];
+
+const getUser = (id) => {
+  return new Promise((resolve, reject) => {
+    const user = users.find((u) => u.id === id);
+
+    if (user) {
+      resolve();
+    } else {
+      reject(`Unable to find user with id of ${id}.`);
+    }
+  });
+};
+
+getUser(2).then((user) => {
+  console.log(user);
+}).catch((e) => {
+  console.log(e);
+});
